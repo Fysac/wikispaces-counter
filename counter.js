d7b39695824674c15d3ff347c8896d370ead4103
Fysac <smcerm@hotmail.com>
2014-04-17 17:11:27 -0400

    Made url customizable

--- counter.js
+++ counter.js
@@ -1,2 +1,3 @@
-var url = 'http://errror.mooo.com/wikispaces/counter/counter.php?username=' + wikispaces_username + "&space=" + wikispaces_spaceName;
-document.write('<iframe src="' + url +'" frameBorder="0" width="200" height="400"><\/iframe>');
+var url = 'http://errror.mooo.com/wikispaces/counter/counter.php';
+var bigurl = url + '?username=' + wikispaces_username + "&space=" + wikispaces_spaceName;
+document.write('<iframe src="' + bigurl +'" frameBorder="0" width="200" height="400"><\/iframe>');
