6961a026433f0c29074b93160f95c726d201983b
Fysac <smcerm@hotmail.com>
2014-04-18 15:19:59 -0400

    Renamed counter.php to index.php

--- counter.js
+++ counter.js
@@ -1,3 +1,3 @@
-var url = 'http://errror.mooo.com/wikispaces/counter/counter.php';
+var url = 'http://errror.mooo.com/wikispaces/counter/index.php';
 var bigurl = url + '?username=' + wikispaces_username + "&space=" + wikispaces_spaceName;
 document.write('<iframe src="' + bigurl +'" frameBorder="0" width="200" height="400"><\/iframe>');
